2a417048890d1bc5e9c6fa5172a7f48e76b8b0db
srikanth <srikanth.yadama@gmail.com>
2018-04-19 18:25:13 -0400

    added config file

--- src/main/java/com/cba/ec2/BuildEC2Client.java
+++ src/main/java/com/cba/ec2/BuildEC2Client.java
@@ -1,5 +1,10 @@
 package com.cba.ec2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.ec2.AmazonEC2;
@@ -10,10 +15,20 @@ public class BuildEC2Client {
 	public BuildEC2Client() {
 		// TODO Auto-generated constructor stub
 	}
+	Properties prop = new Properties();
 	
-	public AmazonEC2 buildEC2ClientCredentials(){
+	
+	public AmazonEC2 buildEC2ClientCredentials() throws Exception{
 		
-		BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJCAH52HV5QH7FPVQ", "1b6dKbHlMO+YNdg4P4nRacaokfAcRzlG9bCjNLjQ");
+		InputStream input = new FileInputStream("config.properties");
+		
+		prop.load(input);
+		
+		
+		String accessKey = prop.getProperty("accessKey");
+		String secretKey = prop.getProperty("secretKey");
+		
+		BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
     	final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard()
     			.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
     			.withRegion("us-east-2")
